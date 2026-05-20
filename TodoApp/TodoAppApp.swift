5eb17f12545669b44a9b0819274e25b2d237e34e
anand NewVision <anand.mandal@newvision-software.com>
2026-05-21 02:19:49 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by NewVision on 21/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
